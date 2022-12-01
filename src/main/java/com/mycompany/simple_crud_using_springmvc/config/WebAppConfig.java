86200a71bd3b561816533518c938343086e6ca72
Ranger-HK <maxravindu99@gmail.com>
2022-12-01 19:48:09 +0530

    WebAppConfig Done

--- src/main/java/com/mycompany/simple_crud_using_springmvc/config/WebAppConfig.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/config/WebAppConfig.java
@@ -1,4 +1,12 @@
 package com.mycompany.simple_crud_using_springmvc.config;
 
-public class WebAppConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.mycompany.simple_crud_using_springmvc" )
+public class WebAppConfig implements WebMvcConfigurer {
 }
