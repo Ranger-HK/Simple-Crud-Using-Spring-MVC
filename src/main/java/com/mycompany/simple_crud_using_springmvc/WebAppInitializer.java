3123c692cf631c94668c3693aa7e5f215eff9d16
Ranger-HK <maxravindu99@gmail.com>
2022-12-01 19:43:52 +0530

    WebAppInitializer  Done

--- src/main/java/com/mycompany/simple_crud_using_springmvc/WebAppInitializer.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/WebAppInitializer.java
@@ -1,4 +1,22 @@
 package com.mycompany.simple_crud_using_springmvc;
 
-public class WebAppInitializer {
+import com.mycompany.simple_crud_using_springmvc.config.WebAppConfig;
+import com.mycompany.simple_crud_using_springmvc.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
