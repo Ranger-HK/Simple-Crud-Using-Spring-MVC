28bb0db20c2f860c0b6e81112a1eda0a61faddda
Ranger-HK <maxravindu99@gmail.com>
2022-12-01 19:49:33 +0530

    WebRootConfig Done

--- src/main/java/com/mycompany/simple_crud_using_springmvc/config/WebRootConfig.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/config/WebRootConfig.java
@@ -1,4 +1,9 @@
 package com.mycompany.simple_crud_using_springmvc.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(JPAConfig.class)
 public class WebRootConfig {
 }
