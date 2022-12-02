2673ed067f89a507f6e07ed6982e3962bd7973d1
Ranger-HK <maxravindu99@gmail.com>
2022-12-02 10:30:34 +0530

    @ResponseStatus(HttpStatus.BAD_REQUEST)

---
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.mycompany.simple_crud_using_springmvc.advisor;
+
+import com.mycompany.simple_crud_using_springmvc.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author atlas
+ * @created 12/2/2022 - 10:28 AM
+ * @project Simple_Crud_Using_SpringMVC
+ */
+
+@RestController
+@CrossOrigin
+public class AppWideExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({Exception.class})
+    public ResponseUtil exceptionHandler(Exception e) {
+        return new ResponseUtil(500, e.getMessage(), null);
+    }
+}
--- src/main/java/com/mycompany/simple_crud_using_springmvc/service/Impl/UserServiceImpl.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/service/Impl/UserServiceImpl.java
@@ -1,11 +1,26 @@
 package com.mycompany.simple_crud_using_springmvc.service.Impl;
 
 import com.mycompany.simple_crud_using_springmvc.dto.UserDTO;
+import com.mycompany.simple_crud_using_springmvc.repo.UserRepo;
 import com.mycompany.simple_crud_using_springmvc.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
+
     @Override
     public void saveUser(UserDTO dto) {
 
