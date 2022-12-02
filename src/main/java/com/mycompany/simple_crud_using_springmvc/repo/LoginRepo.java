a61d470bc77b5703df193614eb49df527c5de076
Ranger-HK <maxravindu99@gmail.com>
2022-12-02 10:03:53 +0530

    repo and uti package done

--- src/main/java/com/mycompany/simple_crud_using_springmvc/repo/LoginRepo.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/repo/LoginRepo.java
@@ -1,4 +1,7 @@
 package com.mycompany.simple_crud_using_springmvc.repo;
 
-public interface LoginRepo {
+import com.mycompany.simple_crud_using_springmvc.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginRepo extends JpaRepository<User,String> {
 }
--- src/main/java/com/mycompany/simple_crud_using_springmvc/util/ResponseUtil.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/util/ResponseUtil.java
@@ -1,4 +1,17 @@
 package com.mycompany.simple_crud_using_springmvc.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class ResponseUtil {
+    private int code;
+    private String message;
+    private Object data;
+
 }
