cfd4ab9847defe309456366651a80c507d0e5a30
Ranger-HK <maxravindu99@gmail.com>
2022-12-02 13:56:37 +0530

    User Controller Done

--- src/main/java/com/mycompany/simple_crud_using_springmvc/controller/UserController.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.mycompany.simple_crud_using_springmvc.controller;
 
+import com.mycompany.simple_crud_using_springmvc.dto.UserDTO;
 import com.mycompany.simple_crud_using_springmvc.service.UserService;
 import com.mycompany.simple_crud_using_springmvc.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,9 +23,23 @@ public class UserController {
 
     }
 
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-//    public
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveUser(@ModelAttribute UserDTO user){
+        userService.saveUser(user);
+        return new ResponseUtil(200, "Ok", null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateUser(@RequestBody UserDTO user){
+        userService.updateUser(user);
+        return new ResponseUtil(200, "Ok", null);
+    }
+
+    @DeleteMapping(params = {"userID"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteUser(@RequestParam String userID){
+        userService.deleteUser(userID);
+        return new ResponseUtil(200, "Ok", null);
+    }
 
 
 }
