ebb8d4708f92358a837b6c16dc51e9cfbc8402b2
Ranger-HK <maxravindu99@gmail.com>
2022-12-02 14:13:28 +0530

    CRUD Done

--- src/main/java/com/mycompany/simple_crud_using_springmvc/repo/UserRepo.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/repo/UserRepo.java
@@ -2,6 +2,10 @@ package com.mycompany.simple_crud_using_springmvc.repo;
 
 import com.mycompany.simple_crud_using_springmvc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepo extends JpaRepository<User,String>{
+public interface UserRepo extends JpaRepository<User, String> {
+
+    @Query(value = "SELECT userID FROM User ORDER BY userID DESC LIMIT 1", nativeQuery = true)
+    String generateUserId();
 }
--- src/main/java/com/mycompany/simple_crud_using_springmvc/service/Impl/UserServiceImpl.java
+++ src/main/java/com/mycompany/simple_crud_using_springmvc/service/Impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.mycompany.simple_crud_using_springmvc.service.Impl;
 
 import com.mycompany.simple_crud_using_springmvc.dto.UserDTO;
+import com.mycompany.simple_crud_using_springmvc.entity.User;
 import com.mycompany.simple_crud_using_springmvc.repo.UserRepo;
 import com.mycompany.simple_crud_using_springmvc.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,26 +25,61 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(UserDTO dto) {
-
+        if (!repo.existsById(dto.getUserID())) {
+            User entity = mapper.map(dto, User.class);
+            repo.save(entity);
+        } else {
+            throw new RuntimeException("User already exists...");
+        }
     }
 
     @Override
     public void deleteUser(String userID) {
-
+        if (repo.existsById(userID)) {
+            repo.deleteById(userID);
+        } else {
+            throw new RuntimeException("Please check the userID.. no such as user...!");
+        }
     }
 
     @Override
     public void updateUser(UserDTO dto) {
-
+        if (repo.existsById(dto.getUserID())) {
+            User entity = mapper.map(dto, User.class);
+            repo.save(entity);
+        } else {
+            throw new RuntimeException("no such as user to update.. please Check the userID...!");
+        }
     }
 
     @Override
     public List<UserDTO> getAllUsers() {
-        return null;
+        List<User> all = repo.findAll();
+        return mapper.map(all, new TypeToken<List<UserDTO>>() {
+
+        }.getType());
     }
 
     @Override
     public String generateUserId() {
-        return null;
+        long count = repo.count();
+        String id = "U00-000";
+
+        if (count != 0) {
+            String generateUserId = repo.generateUserId();
+            int tempId = Integer.parseInt(generateUserId.split("-")[1]);
+            tempId += 1;
+            if (tempId < 10) {
+                id = "U00-00" + tempId;
+            } else if (tempId < 100) {
+                id = "U00-0" + tempId;
+            } else if (tempId < 1000) {
+                id = "U00-" + tempId;
+            }
+        } else {
+            id = "U00-000";
+        }
+        return id;
     }
+
 }
